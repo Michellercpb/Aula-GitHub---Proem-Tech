d32ba44ca01d777e98326042fb57afffcea9feee
Michellercpb <146958097+Michellercpb@users.noreply.github.com>
2024-06-07 17:31:49 -0300

    classe animal adicionada

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("branch suporte");
+
+
+class Animal{
+    constructor(tipo,patas){
+        this.tipo = tipo;
+        this.patas = patas;
+    }
+}
+
+const cahorro = new Animal("mamífero",4);
